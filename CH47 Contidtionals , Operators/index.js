828ba81f4a297e5ccc9a65f3c01efa21245ebc72
Raj Chauhan <craj47577@gmail.com>
2024-05-26 16:54:28 +0530

    Add files via upload

---
+++ CH47 Contidtionals , Operators/index.js
@@ -0,0 +1,89 @@
+console.log("Hello EveryBody");
+let z = 21;
+
+if(z>18){
+    console.log("You can drive");
+}
+else{
+    console.log("You cannot drive");
+}
+
+//OPERATORS
+let a = 2;
+let b = 3;
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a**b)
+// console.log(a%b)
+// console.log("Inc and Dec")
+// a++;
+// console.log(a)
+// a--
+// console.log(a)
+// b++
+// console.log(b)
+// b--
+// console.log(b)
+
+//ASSIGNMENT OPERATORS
+// let r = 2
+// let c = 4;
+// console.log(r+=c)
+// console.log(c-=r)
+// console.log(r*=c)
+// console.log(c/=r)
+// console.log(c%=r)
+// console.log(r**=c)
+// console.log(c=r)
+// console.log(c)
+// console.log()
+// console.log()
+
+//COMAPRISON OPERATOR
+let e = 13
+let n = 2
+console.log(e>n?e-n:n-e) //ternary operators are used to write if-else in one line
+
+// if(e==12){
+//     console.log("Equal to")
+// }
+
+// if(e!=12){
+//     console.log("Not Equal to")
+// }
+// if(e<21){
+//     console.log("Greater than")
+// }
+// if(e>10){
+//     console.log("Smaller than")
+// }
+// if(e<=21){
+//     console.log("Greater than equal to")
+// }
+// if(e>=10){
+//     console.log("Smaller than Equal to")
+// }
+
+//equal value and type operator
+// console.log("21"===21) // '"==='" this operator returns true if value and type of elements are same
+// eg- in this case "21" is string and 21 is int they are differnet in type but values are same ,so O/P will be false
+
+// console.log("21"=="21") //here both value and type are same  , O/P will be true
+
+// console.log("21"!==21) // O/P will be true  , as the are not equal
+
+//LOGICAL OPERATOR
+
+let x = 12;
+let m = 42;
+
+// console.log(x!=m && m!=x);
+// console.log(x==m && m!=x)
+// console.log(x!=m || m==x)
+// console.log(!false)
+// console.log(!true)
+
+
