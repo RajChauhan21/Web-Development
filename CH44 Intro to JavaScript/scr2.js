30a4b5bb6734938707b18384bce2593ac0c7436d
Raj Chauhan <craj47577@gmail.com>
2024-05-26 16:51:07 +0530

    Add files via upload

---
+++ CH44 Intro to JavaScript/scr2.js
@@ -0,0 +1,4 @@
+console.log("System is Running");
+console.log("System is Prunning");
+console.log("System is Sunning");
+console.log("System is Munning");
---
+++ CH44 Intro to JavaScript/script.js
@@ -0,0 +1,19 @@
+alert("hello everyone");
+console.log("System is Running");
+console.log("System is Prunning");
+console.log("System is Sunning");
+console.log("System is Munning");
+
+var cr = prompt("Enter your Name");
+console.log("Your Name is " + cr)
+
+var res = confirm("Are you sure you want to blast your computer because of HER")
+
+if(res){
+    console.log("Blasting your Computer.......")
+}
+else{
+    console.log("Good Decision")
+}
+document.title = "Javascript"
+document.body.style.backgroundColor = "aqua";
